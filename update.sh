351fbae65035eed6360848a0178ffcf9cc8ef3a5
Elliott Allmann <elliott.allmann@cox.net>
2017-12-01 17:03:18 -0800

    Created alias mak for make.
    Signed-off-by: Elliott Allmann <elliott.allmann@cox.net>

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function pushChanges()
+{
+   git add .bash_aliases
+   git commit -asm "Created alias $1 for $2."
+   git push
+   source ~/.bashrc
+}
+
+function pullChanges()
+{
+   git pull
+   source ~/.bashrc
+}
+
+
+if [ -z "$1" ]; then 
+	pullChanges
+else pushChanges $1 $2;
+fi
